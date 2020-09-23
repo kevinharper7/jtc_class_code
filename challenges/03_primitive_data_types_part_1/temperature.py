7389a7f710ab07631a1c5234a891580ce701bcc8
kevinharper7 <kevinharper8@protonmail.com>
2020-09-22 21:34:02 -0400

    temperature.py

---
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -0,0 +1,24 @@
+#converting fahrenheit to celsuis
+celsuis_100 = ( (100 - 32) * 5//9)
+print(celsuis_100)
+
+#converting fahrenheit to celsuis
+Celsuis_0 = ((0 -32) * 5//9)
+print(Celsuis_0)
+
+#converting farenheit to celsuis
+celsuis_34 = (int(34.2- 32)* 5//9)
+print(celsuis_34)
+
+#convertng celsuis to farenheit 
+celsuis_5 = ((5)* 9//5 +32)
+print(celsuis_5)
+
+#which is hotter
+celsuis_30 = (int(30.2)* 9//5 +32)
+print(celsuis_30)
+
+celsuis_85 =(int(85.1 -32) * 5//9)
+print(celsuis_85)
+
+#answer 30.2 celssuis is hotter
